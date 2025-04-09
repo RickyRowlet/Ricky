30bb590489474116b6655453fd158fa0dadfde8a
RickyRowlet <buiphu26080@gmail.com>
2025-04-09 20:29:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from gui import MisaApp
 database.create_files()
 database.init_user_file()
 
-def run_app():
+def run_app(role):
     root = tk.Tk()
-    app = MisaApp(root)
+    app = MisaApp(root, role)  # Truyền quyền (role) vào ứng dụng
     root.mainloop()
 
 if __name__ == "__main__":
